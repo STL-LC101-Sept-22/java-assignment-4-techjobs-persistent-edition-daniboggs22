9e7f131e84d1b613284b233b592d41a5e5482d87
Daneille Boggs <daniboggs22@gmail.com>
2023-03-24 13:29:15 -0500

    Updated Job class and added jobs list in Employer class.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,16 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min=3, max=25, message = "Location must be between 3 and 25 characters in length.")
     public String location;
-
+    @OneToMany
+    @JoinColumn()
+    //employer_id
+    private List<Job> jobs = new ArrayList<>();
     public Employer(){}
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -3,42 +3,28 @@ package org.launchcode.techjobs.persistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job{
-
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
+public class Job extends AbstractEntity{
+    @ManyToOne
+    private Employer employer;
     private String skills;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer employer, String someSkills) {
         super();
-        this.employer = anEmployer;
+        this.employer = employer;
         this.skills = someSkills;
     }
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
-        return employer;
-    }
-
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
+    }
+
+    public Employer getEmployer() {
+        return employer;
     }
 
     public String getSkills() {
